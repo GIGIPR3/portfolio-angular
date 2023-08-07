f0fdd92f11762dde47941c961b7210ec92c53786
luigi <luigidev6464@gmail.com>
2023-08-08 00:15:45 +0200

    .-

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ChisonoComponent } from './chisono/chisono.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  {path: 'chisono', component: ChisonoComponent}
 
 ];
 
